9a7de4a3d2b4d9c6e5e9d7baf209c6eed6a673bc
Kousthubha L Rao <185604287+KousthubhaLRao@users.noreply.github.com>
2026-04-01 21:59:45 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-1

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        long long low = *max_element(weights.begin(), weights.end());     //start of search range
+        long long high = accumulate(weights.begin(), weights.end(), 0);  //end of search range. (shipping everything in one day)
+        int n = weights.size();
+        if(n == days) return low;
+
+        long long mid, res;
+
+        while(low <= high)
+        {
+            mid = (low + high) / 2; //capacity that'll be tested
+
+            int sum = 0, count = 0, i = 0;
+            while(i < n)
+            {
+                if(sum + weights[i] <= mid)
+                {
+                    sum += weights[i];
+                    i++;
+                }
+                else
+                {
+                    count++; //count number of days
+                    sum = 0; //reset total package weight
+                }
+            }
+            count++; //apparently the last day isn't being considered. so this line.
+
+            if(count <= days)
+            {
+                res = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return res;
+    }
+};
