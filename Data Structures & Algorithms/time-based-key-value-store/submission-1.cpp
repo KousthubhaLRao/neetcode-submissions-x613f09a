63ffc4ad1515cac0929e1697469aabf0a9fb5dbf
Kousthubha L Rao <185604287+KousthubhaLRao@users.noreply.github.com>
2026-04-04 20:04:37 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,29 @@
+class TimeMap {
+public:
+    TimeMap() {}
+    unordered_map<string, vector<pair<string, int>>> mp;
+
+    void set(string key, string value, int timestamp) {
+        mp[key].emplace_back(value, timestamp);
+    }
+    
+    string get(string key, int timestamp) {
+        if(!mp.count(key)) return "";
+
+        auto &arr = mp[key];
+        int l = 0, r = arr.size() - 1, mid; 
+        string res;
+
+        while(l <= r)
+        {
+            mid = (l + r) / 2;
+            if(arr[mid].second <= timestamp)
+            {
+                res = arr[mid].first;
+                l = mid + 1;
+            }
+            else r = mid - 1;
+        }
+        return res;
+    }
+};
