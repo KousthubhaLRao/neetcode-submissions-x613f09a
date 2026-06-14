83a9b3aefa0f1462039363e551acade6765e2e9a
Kousthubha L Rao <185604287+KousthubhaLRao@users.noreply.github.com>
2026-06-15 02:02:38 +0530

    Add: island-perimeter - submission-1

---
+++ Data Structures & Algorithms/island-perimeter/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int p = 0; //perimeter
+
+        for(int i = 0; i < grid.size(); i++)
+        {
+            for(int j = 0; j < grid[0].size(); j++)
+            {
+                if(grid[i][j] == 0) continue; //i don't want anything to do with lake cells
+
+                p += 4; //else
+                if(i and grid[i-1][j]) p -= 2; //If the cell above is also land, subtract 2.
+                if(j and grid[i][j-1]) p -= 2;  //If the cell to the left is also land, subtract 2.
+            }
+        }
+        return p;
+    }
+};
